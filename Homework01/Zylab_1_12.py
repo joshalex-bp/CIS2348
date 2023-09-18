238609d698ebe00787af80be12c210bafe456ac8
joshalex-bp <143367254+joshalex-bp@users.noreply.github.com>
2023-09-17 23:08:22 -0500

    Adding Zylab 1.12

---
+++ Homework01/Zylab_1_12.py
@@ -0,0 +1,9 @@
+# Joshua Guajardo    PISD:1811892
+
+# Requesting input from user
+userNum = int(input())
+# Formula used to calculate the square of a number
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+# Outputting the square of the input
+print(userNumSquared) # Output formatting issue here; fix it when instructed
