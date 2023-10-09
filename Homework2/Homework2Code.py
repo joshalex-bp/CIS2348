31c03f8c70b301299a86079f5353d64a5859e54c
joshalex-bp <143367254+joshalex-bp@users.noreply.github.com>
2023-10-08 23:47:55 -0500

    Initial Commit

---
+++ Homework2/Homework2Code.py
@@ -0,0 +1,42 @@
+# Joshua Guajardo   PSID:1811892
+
+
+
+import datetime
+
+def date_pass(string_date):
+    month_date = string_date.find(" ")
+    day_date = string_date.find(",") + 2
+    year_date = string_date.find(",") + 2
+
+    if month_date != -1 and day_date != -1 and year_date != -1:
+        month_new = string_date[:month_date]
+        day_new = string_date[month_date + 1: day_date - 2]
+        year_new = string_date[year_date:]
+        return month_new, day_new, year_new
+    else:
+        month_new = 1
+        day_new = 1
+        year_new = 1
+        return month_new, day_new, year_new
+
+
+today = datetime.datetime.today()
+while True:
+    int_date = input()
+    if int_date == '-1':
+        break
+
+    month_new, day_new, year_new = date_pass(int_date)
+
+    if month_new == 1 or day_new == 1 or year_new == 1:
+        int_date = input()
+    else:
+        date_new_format = datetime.datetime.strptime(f"{month_new} {day_new}, {year_new}", "%B %d, %Y")
+
+
+    if date_new_format < today:
+        date = date_new_format.strftime("%m/%d/%Y")
+        print(date)
+
+
