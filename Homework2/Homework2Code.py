94d7a13d78f29f1e56660c4389d5b30368c96013
joshalex-bp <143367254+joshalex-bp@users.noreply.github.com>
2023-10-08 23:54:18 -0500

    Added a way for the code to read "inputDates.txt"

--- Homework2/Homework2Code.py
+++ Homework2/Homework2Code.py
@@ -22,6 +22,16 @@ def date_pass(string_date):
 
 
 today = datetime.datetime.today()
+
+int_file = "inputDates.txt"
+
+
+with open(int_file, "r") as file:
+    for line in file:
+        str_date = line.strip()
+        if str_date == '-1':
+            break
+
 while True:
     int_date = input()
     if int_date == '-1':
@@ -30,7 +40,7 @@ while True:
     month_new, day_new, year_new = date_pass(int_date)
 
     if month_new == 1 or day_new == 1 or year_new == 1:
-        int_date = input()
+        continue
     else:
         date_new_format = datetime.datetime.strptime(f"{month_new} {day_new}, {year_new}", "%B %d, %Y")
 
